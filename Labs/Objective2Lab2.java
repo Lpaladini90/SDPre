1bbd23750766b0df505a06ef272cb8876b1a5e6d
Lpaladini90 <lpaladini@me.com>
2022-01-14 08:36:58 -0800

    name age state

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Lucas Paladini";
+    int age = 31;
+    String state = "Florida";
+      System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+
+  }
+}
