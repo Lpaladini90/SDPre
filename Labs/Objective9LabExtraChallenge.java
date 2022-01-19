874bc63d7bf3869f183e21c0696ee0c567a69ae8
Lpaladini90 <lpaladini@me.com>
2022-01-19 14:03:23 -0800

    Extra Challenge)

---
+++ Labs/Objective9LabExtraChallenge.java
@@ -0,0 +1,45 @@
+public class Objective9LabExtraChallenge {
+  public static void main(String[] args) {
+    int i=1;
+
+    while(i<=10) {
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      i++;
+
+
+  }
+    for(i=1; i<=10; i+=1){
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+    }
+
+}
+}
+
+
+
+
+
+
+
+
+
+    //printRectangle();
+
+  //}
+
+  //public static void printSquare() {
+    /*  System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+*/
+    //}
+    //public static void printRectangle(){
+          //printSquare();
+          //printSquare();
