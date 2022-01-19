9896919e5f08a98fe6b9a58cf103b71e1527204e
Lpaladini90 <lpaladini@me.com>
2022-01-19 10:11:30 -0800

    edit

--- Labs/Objective8Lab1.java
+++ Labs/Objective8Lab1.java
@@ -1,19 +1,19 @@
-//import java.util.Scanner;
+
 
 public class Objective8Lab1 {
   public static void main(String[] args) {
 
-  //  Scanner scanner = new Scanner(System.in);
 
-    for (int i=1; i < 11; i+=1) {
+
+    for (int i=1; i <= 10; i++) {
       System.out.println(i);
     }
 
-    for (int i =10; i>0; i-=1) {
+    for (int i =10; i>=0; i--) {
       System.out.println(i);
     }
 
-    
+
 
 
   }
